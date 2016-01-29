896555a54b54e94888be9ae2e3d416fbcc8f9faf
Andrés notami <andresnotami@gmail.com>
2016-01-29 11:58:39 -0500

    Lets Got

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appYoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appYoApp
+ */
+angular.module('appYoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appYoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appYoApp
+ */
+angular.module('appYoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
